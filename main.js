27eda9859680eed23124a349e3846d2ce245137b
Bruno Costa <bcosta17.bc@gmail.com>
2023-09-20 16:44:26 -0300

    finished

---
+++ main.js
@@ -0,0 +1,58 @@
+// Exercício 1
+alert("Hello World!");
+
+// Exercício 2
+let num1 = 5;
+let num2 = 7;
+let resultadoSoma = num1 + num2;
+console.log("Resultado da soma:", resultadoSoma);
+
+// Exercício 3
+let value = 10;
+if (typeof value === "number") {
+  console.log("É um número");
+} else {
+  console.log("Não é um número");
+}
+
+// Exercício 4
+let text = "Olá";
+if (typeof text === "string") {
+  console.log("É uma string");
+} else {
+  console.log("Não é uma string");
+}
+
+// Exercício 5
+let booleanValue = true;
+if (typeof booleanValue === "boolean") {
+  console.log("É um booleano");
+} else {
+  console.log("Não é um booleano");
+}
+
+// Exercício 6
+console.log( num1 - num2);
+
+// Exercício 7
+console.log(num1 * num2);
+
+// Exercício 8
+console.log(num1 / num2);
+
+// Exercício 9
+let evenNumber = 8;
+if (evenNumber % 2 === 0) {
+  console.log("É um número par");
+} else {
+  console.log("Não é um número par");
+}
+
+// Exercício 10
+let oddNumber = 7;
+if (oddNumber % 2 !== 0) {
+  console.log("É um número ímpar");
+} else {
+  console.log("Não é um número ímpar");
+}
+
